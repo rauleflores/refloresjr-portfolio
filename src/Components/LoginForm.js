1d68cd8115a6664254ceedc3046068b6fc44fc5a
Raul <raulefloresjr@gmail.com>
2021-03-10 20:30:42 -0800

    Progress on Login Page.

---
+++ src/Components/LoginForm.js
@@ -0,0 +1,62 @@
+import React from "react";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { Button, TextField } from "@material-ui/core";
+
+let validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Enter a valid email.")
+    .required("Email is required.")
+    .min(15, "Email is too short - 4 chars minimum.")
+    .max(50, "Email is too long."),
+  password: yup
+    .string("Please enter a password.")
+    .min(6, "Password is too short! 6 char minimum.")
+    .max(24, "Password is too long. 24 char maximum."),
+});
+
+const LoginForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <p>Login</p>
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="E-mail"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/Components/Splash.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Splash = () => {
+  return (
+    <div>
+      <h1>Raul Flores</h1>
+      <div>
+        <div></div>
+        <div>
+          <h3>Guest Info</h3>
+          <p>
+            E-mail: <span></span>
+          </p>
+          <p>
+            Password: <span></span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Splash;

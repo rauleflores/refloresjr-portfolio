57aa3e73821fece16645087b5fde3610b0f58540
Raul <raulefloresjr@gmail.com>
2021-03-14 15:16:21 -0700

    added a barebones dashboard

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import "./Styles/App.css";
+import NoMatch from "./NoMatch";
 import Splash from "./Components/Splash";
 import SignUp from "./Components/SignUpForm";
+import Dashboard from "./Components/Dashboard";
 import LoginForm from "./Components/LoginForm";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Route path="/">
-      <div className="App">
-        <div className="left-side">
-          <Splash />
-        </div>
-        <div className="right-side">
-          <Switch>
-            <Route path="/signup-page">
-              <SignUp />
-            </Route>
-            <Route path="/login-page">
-              <LoginForm />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Route>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <div className="FrontPage">
+            <div className="left-side">
+              <Splash />
+            </div>
+            <div className="right-side">
+              <Switch>
+                <Route path="/signup-page">
+                  <SignUp />
+                </Route>
+                <Route path="/login-page">
+                  <LoginForm />
+                </Route>
+              </Switch>
+            </div>
+          </div>
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
+    </>
   );
 }
 

5831d759334dd4c8959ce3cddef9301550a53660
Raul <raulefloresjr@gmail.com>
2021-03-14 22:15:30 -0700

    NavBar nearing completion

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,11 +6,25 @@ import {
   Toolbar,
   IconButton,
   Typography,
+  makeStyles,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 
+const useStyles = makeStyles((theme) => ({
+  //   root: {
+  //     flexGrow: 1,
+  //   },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 const NavBar = (props) => {
+  const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
 
@@ -23,37 +37,46 @@ const NavBar = (props) => {
   };
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6">Raul E. Flores Jr</Typography>
-        <div>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
           <IconButton
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleMenu}
+            edge="start"
+            className={classes.menuButton}
             color="inherit"
+            aria-label="menu"
           >
-            <AccountCircle />
+            <MenuIcon />
           </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorEl}
-            anchorOrigin={{ veritcal: "top", horizontal: "right" }}
-            keepMounted
-            transformOrigin={{ veritcal: "top", horizontal: "right" }}
-            open={open}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>My Profile</MenuItem>
-            <MenuItem onClick={handleClose}>My Account</MenuItem>
-          </Menu>
-        </div>
-      </Toolbar>
-    </AppBar>
+          <Typography variant="h6" className={classes.title}>
+            Raul E. Flores Jr
+          </Typography>
+          <div>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{ veritcal: "top", horizontal: "right" }}
+              keepMounted
+              transformOrigin={{ veritcal: "top", horizontal: "right" }}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>My Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My Account</MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
